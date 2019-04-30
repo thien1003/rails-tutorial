4f3563f312ecc624321b6f154beecdcfc6dffde2
thiennt <thiennt@bap.jp>
2019-04-30 12:51:39 +0700

    Customize views

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_04_24_161554) do
+ActiveRecord::Schema.define(version: 2019_04_30_034100) do
 
   create_table "microposts", force: :cascade do |t|
     t.text "content"
